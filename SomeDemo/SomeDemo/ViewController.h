c4ac9db2df0fad4be90e6d301a72082a069e98a1
haozp <leo@leodemacbook-pro.local>
2016-09-12 16:39:23 +0800

    初始化工程someDemo

---
+++ SomeDemo/SomeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SomeDemo
+//
+//  Created by leo on 16/9/12.
+//  Copyright © 2016年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SomeDemo/SomeDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SomeDemo
+//
+//  Created by leo on 16/9/12.
+//  Copyright © 2016年 haozp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"创建Somedemo");
+}
+
+@end
